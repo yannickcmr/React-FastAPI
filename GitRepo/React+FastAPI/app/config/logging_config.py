a8fc3cec0381e3f63f27513c60d58f39e34cb612
Yannick <88460673+yannickcmr@users.noreply.github.com>
2025-12-20 22:11:20 +0100

    Delete GitRepo directory

--- GitRepo/React+FastAPI/app/config/logging_config.py
+++
@@ -1,7 +0,0 @@
-""" Default Logging Config """
-import logging
-
-def create_logger() -> logging.Logger:
-    """ Generate Default Logging Setup """
-    logging.basicConfig(level=logging.INFO)
-    return logging.getLogger(__name__)
